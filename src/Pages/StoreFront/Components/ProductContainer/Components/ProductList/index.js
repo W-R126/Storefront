9c9b4f52a33eeab05fa0779965d4ffd9aff7a005
rz-roy <rz.royzhang@gmail.com>
2020-09-17 18:34:07 +0300

    remove group product by category when category all

--- src/Pages/StoreFront/Components/ProductContainer/Components/ProductList/index.js
+++ src/Pages/StoreFront/Components/ProductContainer/Components/ProductList/index.js
@@ -13,11 +13,9 @@ import ProductCard from '../ProductCard';
 import { GET_STORE_SETTING_PRODUCT } from '../../../../../../graphql/store/store-query';
 import { GET_MERCHANT_NET_PRICE } from '../../../../../../graphql/merchant/merchant-query';
 import { GET_CURRENCY } from '../../../../../../graphql/localisation/localisation-query';
-import { GET_CATEGORIES } from '../../../../../../graphql/categories/categories-query';
 
 import { getIsShowSideCategory } from '../../../../../../utils/store';
 import { getOrdredProducts } from '../../../../../../utils/product';
-import { getOrderedCategories } from '../../../../../../utils/category';
 import { getProductPaginationAction } from '../../../../../../actions/productAction';
 
 const ProductList = ({ client, getProductPaginationAction, loading }) => {
@@ -36,7 +34,6 @@ const ProductList = ({ client, getProductPaginationAction, loading }) => {
   );
   const { data: merchantNetPrice } = useQuery(GET_MERCHANT_NET_PRICE);
   const { data: currencyData } = useQuery(GET_CURRENCY);
-  const { data: categoryList, loading: categoryLoading, error } = useQuery(GET_CATEGORIES);
 
   useEffect(() => {
     getProductPaginationAction(client, filter, pageData);
@@ -69,29 +66,29 @@ const ProductList = ({ client, getProductPaginationAction, loading }) => {
     return renderCards;
   };
 
-  const getGroupedCategory = () => {
-    const orderedProducts = getOrdredProducts(productList, storeSettingData);
-    if (filter.category === 'all') {
-      if (!getIsShowSideCategory(storeSettingData)) return orderedProducts;
-      const orderedCategory = getOrderedCategories(categoryList, storeSettingData);
-      if (orderedCategory.length === 0) return orderedProducts;
-      let groupProducts = [];
-      orderedCategory.forEach((item) => {
-        const filteredProduct = orderedProducts.filter((itemOne) => itemOne.category.id === item.id);
-        groupProducts = [...groupProducts, ...filteredProduct];
-      });
-      return groupProducts;
-    } else {
-      return orderedProducts;
-    }
-  };
+  // const getGroupedCategory = () => {
+  //   const orderedProducts = getOrdredProducts(productList, storeSettingData);
+  //   if (filter.category === 'all') {
+  //     if (!getIsShowSideCategory(storeSettingData)) return orderedProducts;
+  //     const orderedCategory = getOrderedCategories(categoryList, storeSettingData);
+  //     if (orderedCategory.length === 0) return orderedProducts;
+  //     let groupProducts = [];
+  //     orderedCategory.forEach((item) => {
+  //       const filteredProduct = orderedProducts.filter((itemOne) => itemOne.category.id === item.id);
+  //       groupProducts = [...groupProducts, ...filteredProduct];
+  //     });
+  //     return groupProducts;
+  //   } else {
+  //     return orderedProducts;
+  //   }
+  // };
 
   return (
     <Grid container className={classes.root} style={{}}>
       <Grid item xs={12}>
         <Grid container spacing={2}>
           {productLoading && pageData.total_pages === 0 && renderLoadingCards()}
-          {getGroupedCategory().map((item, nIndex) => {
+          {getOrdredProducts(productList, storeSettingData).map((item, nIndex) => {
             return (
               <Grid item lg={getIsShowSideCategory(storeSettingData) ? 4 : 3} md={6} xs={12} key={nIndex}>
                 <ProductCard
